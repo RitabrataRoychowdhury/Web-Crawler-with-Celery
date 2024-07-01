c64dd47730816d11e32c49437d2691a67514754f
Ritabrata Roychowdhury <91773580+RitabrataRoychowdhury@users.noreply.github.com>
2024-07-01 15:23:23 +0530

    Add files via upload

---
+++ parserlist.py
@@ -0,0 +1,18 @@
+from urllib.parse import urlparse
+from parsers import defaults, scrapemelive, quotestoscrape
+
+
+parsers = {
+    'scrapeme.live': scrapemelive,
+    'quotes.toscrape.com': quotestoscrape,
+}
+
+
+def get_parser(url):
+    hostname = urlparse(url).hostname  # extract domain from URL
+
+    if hostname in parsers:
+        # use the dict above to return the custom parser if present
+        return parsers[hostname]
+
+    return defaults
